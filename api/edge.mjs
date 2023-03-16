6642bdf6d0cb115dc825fad3e949beb37b38408b
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-03-16 19:46:49 +0500

    Initial prototype

---
+++ api/edge.mjs
@@ -0,0 +1,5 @@
+export const config = {runtime: "edge"};
+
+const headers = {"Content-Type": "text/json; charset=utf-8"};
+
+export default () => new Response(JSON.stringify(process.env), {headers});
